c527ef68937559d43f29946ae6914e57bd7486a7
Matthew Treinish <mtreinish@kortar.org>
2018-06-07 19:08:00 -0400

    License project under GPLv3
    The project was missing it's license file, this commit corrects that and
    adds the licensing for GPLv3 where needed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// Copyright 2018 Matthew Treinish
+//
+// This file is part of junitxml2subunit
+//
+// junitxml2subunit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// junitxml2subunit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with junitxml2subunit.  If not, see <http://www.gnu.org/licenses/>.
+
 extern crate chrono;
 extern crate minidom;
 extern crate subunit_rust;
