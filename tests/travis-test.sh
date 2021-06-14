e1f7d3f0b49e6cf2eb7669075efc4d7b554cb0c8
Matthew Treinish <mtreinish@kortar.org>
2021-06-14 09:42:47 -0400

    Pivot CI to github actions (#9)
    With the tight travis quotas for open source projects now it is not a
    viable provider for running CI on this project anymore. While this
    project is relatively low activity (as it's mostly stable at this point)
    the quota is shared on my github account and other projects eat all the
    availble quota. This commit switches CI to use github actions which
    doesn't have this limitation.

--- tests/travis-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-pushd $parent_path > /dev/null
-test-venv/bin/python -m subunit.run test_command | test-venv/bin/subunit-trace
-popd > /dev/null
